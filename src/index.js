2f3eeb24d2410533542f098ccce0354129831b8b
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-13 20:33:18 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const PORT = 3000;
+
+const setupAndStartServer = async () => {
+  const app = express();
+  app.listen(PORT, () => {
+    console.log(`Server started at ${PORT}`);
+  });
+};
+setupAndStartServer();
