7226ebedb1fa57bc907bfbc47831425f1f72d9f3
Vikash Kumar <vikashk.ug21.cs@nitp.ac.in>
2024-01-15 13:43:14 +0530

    Added city-service

---
+++ src/services/city-service.js
@@ -0,0 +1,53 @@
+const { CityRepository } = require("../repository/city-repository");
+
+
+class CityService {
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(cityData) {
+        try {
+            const city = await this.cityRepository.createCity(cityData);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at service layer.");
+            throw{error};
+        }
+    }
+
+    async deleteCity(cityId) {
+        try {
+            const deletedcityResponse = await this.cityRepository.deleteCity(cityId);
+            return deletedcityResponse;
+        } catch (error) {
+            console.log("Something went wrong at service layer.");
+            throw{error};
+        }
+    }
+
+    async updateCity(cityId,cityData) {
+        try {
+            const cityResponse = await this.cityRepository.updateCity(
+                cityId,
+                cityData
+            );
+            return cityResponse;
+        } catch (error) {
+            console.log("Something went wrong at service layer.");
+            throw{error};
+        }
+    }
+
+    async getCity(cityId) {
+        try {
+            const city = this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Something went wrong at service layer.");
+            throw{error};
+        }
+    }
+}
+
+module.exports = CityService;
